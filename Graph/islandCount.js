a4f9b54ec9ef9d8d86374ce68d2c8e20f90aafa6
mamadou <mamadou@openfn.org>
2021-09-27 21:15:22 +0000

    count the number of island in a grid

---
+++ Graph/islandCount.js
@@ -0,0 +1,31 @@
+const islandCount = (grid) => {
+  let count = 0;
+  const visited = new Set();
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      if (explore(grid, r, c, visited)) {
+        count += 1;
+      }
+    }
+  }
+
+  return count;
+};
+
+const explore = (grid, r, c, visited) => {
+  const rowInbounds = 0 <= r && r < grid.length;
+  const colInbounds = 0 <= c && c < grid[0].length;
+  if (!rowInbounds || !colInbounds) return false;
+
+  if (grid[r][c] === "W") return false;
+
+  if (visited.has(r + "," + c)) return false;
+  visited.add(r + "," + c);
+
+  explore(grid, r - 1, c, visited);
+  explore(grid, r + 1, c, visited);
+  explore(grid, r, c - 1, visited);
+  explore(grid, r, c + 1, visited);
+
+  return true;
+};
